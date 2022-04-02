608805fd96cd6bf606d302ed9e0957023c6d83f1
arodriguez133 <arodriguez133@icloud.com>
2022-04-01 21:04:43 -0500

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,36 +2,46 @@ const router = require('express').Router();
 const { createToken } = require('../secrets');
 const bcrypt = require('bcryptjs');
 const { add } = require('./model')
+const { checkUsernameFree } = require('../middleware/checkUsernameFree')
+const { checkPayload } = require('../middleware/checkPayLoad');
+const { checkUsernameExists } = require('../middleware/checkUsernameExists')
 
-router.post('/register', async (req, res, next) => {
-  try {
-    const hash = bcrypt.hashSync(req.body.password, 2);
-    const newUser = await add({
-      username: req.body.username,
-      password: hash,
-    });
-    res.status(200).json(newUser);
-  } catch (error) {
-    next(error)
-  }
-
-});
-
-router.post('/login', async (req, res, next) => {
-  try {
-    const verified = bcrypt.compareSync(
-      req.body.password,
-      req.userData.password
-    );
-    if (verified) {
-      const token = createToken(req.userData)
-      res.status(200).json({ message: `welcome ${req.body.username}!`, token })
-    } else {
-      res.status(401).json({ message: "invalid credentials" })
+router.post(
+  '/register',
+  checkPayload,
+  checkUsernameFree,
+  async (req, res, next) => {
+    try {
+      const hash = bcrypt.hashSync(req.body.password, 2);
+      const newUser = await add({
+        username: req.body.username,
+        password: hash,
+      });
+      res.status(200).json(newUser);
+    } catch (error) {
+      next(error)
     }
-  } catch (error) {
-    next(error)
-  }
-});
+  });
+
+router.post(
+  '/login',
+  checkPayload,
+  checkUsernameExists,
+  async (req, res, next) => {
+    try {
+      const verified = bcrypt.compareSync(
+        req.body.password,
+        req.userData.password
+      );
+      if (verified) {
+        const token = createToken(req.userData)
+        res.status(200).json({ message: `welcome ${req.body.username}!`, token })
+      } else {
+        res.status(401).json({ message: "invalid credentials" })
+      }
+    } catch (error) {
+      next(error)
+    }
+  });
 
 module.exports = router;
